82b3bd437f4ee37da334b221dccc19511baa9321
[aadler4] <[gleeb101@gmail.com]>
2018-04-03 11:12:48 -0800

    Simple program
    Made a drawing of a guy and made the ability to move him around on a picture. Not sure how different IDE's handle the pictures, for Xcode, it just stores the various supporting files in a "Resource" folder - hopefully everyone can figure out how their "Resource" folder works

--- main.cpp
+++ main.cpp
@@ -1,25 +1,55 @@
 //This is Liam adding a comment and committing
 #include <SFML/Graphics.hpp>
+#include "ResourcePath.hpp"
 
-int main()
+int main ()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+    sf::CircleShape shape(50);
+    shape.setFillColor(sf::Color::Yellow);
+    
+    sf::Texture texture;
+    if (!texture.loadFromFile(resourcePath() + "Drawing.png")) {
+        return EXIT_FAILURE;
+    }
+    sf::Sprite guy(texture);
+    guy.setScale(.25, .25);
+    
+    sf::Texture background;
+    if (!background.loadFromFile(resourcePath() + "cute_image.jpg")) {
+        return EXIT_FAILURE;
+    }
+    sf::Sprite background1(background);
+    background1.setScale(1.5,1.5);
+    
     while (window.isOpen())
     {
+        window.clear();
+        window.draw(background1);
+        window.draw(shape);
+        window.draw(guy);
+        
         sf::Event event;
         while (window.pollEvent(event))
         {
+            int moveValue = 25;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+                guy.move(-moveValue,0);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+                guy.move(moveValue,0);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+                guy.move(0,-moveValue);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+                guy.move(0,moveValue);
+            }
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
-        window.clear();
-        window.draw(shape);
         window.display();
     }
-
+    
     return 0;
 }
