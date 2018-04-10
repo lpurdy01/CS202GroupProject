cc1851eba668a386b220e99bc9a6962700415d44
[aadler4] <[gleeb101@gmail.com]>
2018-04-10 10:33:57 -0800

    changes made to main

---
+++ GameBase.cpp
@@ -0,0 +1,9 @@
+//
+//  GameBase.cpp
+//  CS202FinalLocal
+//
+//  Created by Andrew Adler on 4/10/18.
+//  Copyright © 2018 Andrew Adler. All rights reserved.
+//
+
+#include "GameBase.hpp"
---
+++ GameBase.hpp
@@ -0,0 +1,11 @@
+// GameBase.hpp
+// File containing data for everything within the game environment
+//     -Can be seperated to multiple hpp's / cpp's if we need to later
+
+
+#ifndef GameBase_hpp
+#define GameBase_hpp
+
+#include <stdio.h>
+
+#endif /* GameBase_hpp */
