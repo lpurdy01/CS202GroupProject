2d63e1f1ddcc4145710ea10b1fd118f88a6c624d
[aadler4] <[gleeb101@gmail.com]>
2018-04-12 13:19:08 -0800

    Rearranged the rearrange - now there are runOfflineGame and runClientGame functions

--- main.cpp
+++ main.cpp
@@ -4,6 +4,101 @@
 #include "ResourceFunctions.hpp"
 #include "Network.hpp"
 
+void runOfflineGame () {
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+    //sf::CircleShape shape(50);
+    //shape.setFillColor(sf::Color::Yellow);
+    
+    //system("dir"); //Place Game Resources in this path
+    
+    
+    Character guy("Drawing.png");
+    guy.setScale(.25, .25);
+    
+    Background bg("cute_image.jpg");
+    
+    while (window.isOpen())
+    {
+        window.clear();
+        window.draw(bg);
+        window.draw(guy);
+        
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            int moveValue = 25;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            {
+                guy.move(-moveValue,0);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            {
+                guy.move(moveValue,0);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            {
+                guy.move(0,-moveValue);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            {
+                guy.move(0,moveValue);
+            }
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        
+        window.display();
+    }
+    return 0;
+}
+
+void runClientGame () {
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+    //sf::CircleShape shape(50);
+    //shape.setFillColor(sf::Color::Yellow);
+    
+    //system("dir"); //Place Game Resources in this path
+    
+    
+    Character guy("Drawing.png");
+    guy.setScale(.25, .25);
+    
+    Background bg("cute_image.jpg");
+    
+    while (window.isOpen())
+    {
+        window.clear();
+        window.draw(bg);
+        window.draw(guy);
+        
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            int moveValue = 25;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+            {
+                guy.move(-moveValue,0);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+            {
+                guy.move(moveValue,0);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            {
+                guy.move(0,-moveValue);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+            {
+                guy.move(0,moveValue);
+            }
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        
+        window.display();
+    }
+    return 0;
+}
 
 
 int main ()
@@ -45,104 +140,12 @@ int main ()
         string ip = getLine("Please enter ip of server"); //Make sure to add check for valid IP
         NetworkClient client;
         if(client.connect(ip)){
-        sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
-        //sf::CircleShape shape(50);
-        //shape.setFillColor(sf::Color::Yellow);
-
-        //system("dir"); //Place Game Resources in this path
-
-
-        Character guy("Drawing.png");
-        guy.setScale(.25, .25);
-
-        Background bg("cute_image.jpg");
-
-        while (window.isOpen())
-        {
-            window.clear();
-            window.draw(bg);
-            window.draw(guy);
-
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                int moveValue = 25;
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-                {
-                    guy.move(-moveValue,0);
-                }
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-                {
-                    guy.move(moveValue,0);
-                }
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-                {
-                    guy.move(0,-moveValue);
-                }
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-                {
-                    guy.move(0,moveValue);
-                }
-                if (event.type == sf::Event::Closed)
-                    window.close();
-            }
-
-            window.display();
-        }
+            runClientGame();
         }
         return 0;
     }
     else if(consoleType == type::Offline)
     {
-        std::cout << "Entering Offline Mode" << std::endl;
-        sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
-        //sf::CircleShape shape(50);
-        //shape.setFillColor(sf::Color::Yellow);
-
-        //system("dir"); //Place Game Resources in this path
-
-
-        Character guy("Drawing.png");
-        guy.setScale(.25, .25);
-
-        Background bg("cute_image.jpg");
-
-        while (window.isOpen())
-        {
-            window.clear();
-            window.draw(bg);
-            window.draw(guy);
-
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                int moveValue = 25;
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-                {
-                    guy.move(-moveValue,0);
-                }
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-                {
-                    guy.move(moveValue,0);
-                }
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-                {
-                    guy.move(0,-moveValue);
-                }
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-                {
-                    guy.move(0,moveValue);
-                }
-                if (event.type == sf::Event::Closed)
-                    window.close();
-            }
-
-            window.display();
-        }
-        return 0;
+        runOfflineGame();
     }
-
-
-
-
 }
